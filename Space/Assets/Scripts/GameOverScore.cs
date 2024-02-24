da998cabcc96ba16fef102b3d92c994441bec1b8
sammydoodles <samarthk2402@gmail.com>
2024-02-24 15:36:15 +0000

    Polished game

--- Space/Assets/Scripts/GameOverScore.cs
+++ Space/Assets/Scripts/GameOverScore.cs
@@ -2,20 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-public class Score : MonoBehaviour
+
+public class GameOverScore : MonoBehaviour
 {
     private TextMeshProUGUI textMeshPro;
-    public int score = 0;
     // Start is called before the first frame update
     void Start()
     {
         textMeshPro = GetComponent<TextMeshProUGUI>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        PlayerPrefs.SetInt("Score", score);
-        textMeshPro.text = PlayerPrefs.GetInt("Score").ToString();
+        textMeshPro.text = "Score: " + PlayerPrefs.GetInt("Score").ToString();
     }
 }
--- Space/Assets/Scripts/Health.cs
+++ Space/Assets/Scripts/Health.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Health : MonoBehaviour
 {
@@ -10,9 +12,14 @@ public class Health : MonoBehaviour
     public GameObject heart2;
     public GameObject heart3;
 
-    public float opacity1;
-    public float opacity2;
-    public float opacity3;
+    public float intensity1;
+    public float intensity2;
+    public float intensity3;
+
+    public VolumeProfile volumeProfile;
+    public float intensity = 0f;
+
+    Vignette vignette;
 
     //public GameObject damage;
 
@@ -20,11 +27,27 @@ public class Health : MonoBehaviour
 
     void Start(){
         //dRenderer = damage.GetComponent<Renderer>();
+        if (volumeProfile == null)
+        {
+            Debug.LogError("Volume Profile is not assigned.");
+            return;
+        }
+
+        // Try to get the Vignette component from the volume profile
+        if (!volumeProfile.TryGet(out vignette))
+        {
+            Debug.LogError("Vignette is not found in the Volume Profile.");
+            return;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (vignette != null)
+        {
+            vignette.intensity.value = intensity;
+        }
         switch(health){
             case 0:
                 heart3.SetActive(false);
@@ -36,18 +59,21 @@ public class Health : MonoBehaviour
                 heart3.SetActive(false);
                 heart2.SetActive(false);
                 heart1.SetActive(true);
+                intensity = intensity3;
                 // SetOpacity(opacity1);
                 break;
             case 2:
                 heart3.SetActive(false);
                 heart2.SetActive(true);
                 heart1.SetActive(true);
+                intensity = intensity2;
                 // SetOpacity(opacity2);
                 break;
             case 3:
                 heart3.SetActive(true);
                 heart2.SetActive(true);
                 heart1.SetActive(true);
+                intensity = intensity1;
                 // SetOpacity(opacity3);
                 break;
         }
